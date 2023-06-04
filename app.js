1d1c21aae6d405064e909fe3e0a1c19a39be86f4
Kost Klim <kostkli1@mail.ru>
2023-06-04 12:28:03 +0300

    add port+uid

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
+const os = require("os");
+const userInfo = os.userInfo();
+const uid = userInfo.uid;
+const hostname = '0.0.0.0';
+const port = 1000 + uid;
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
